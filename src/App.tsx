96b588d1e36e95908c36619cc0686306595b4fb0
Alexander Malfait <alexander.malfait@catsolutions.be>
2021-03-11 23:12:46 +0100

    Add react-router, basic dishes page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
-import './App.css';
-import {AppBar, Container, Toolbar, Typography} from "@material-ui/core";
+import './App.scss';
+import {AppBar, Container, IconButton, Toolbar, Typography} from "@material-ui/core";
 import {Planner} from "./planner/Planner";
+import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
+import {CalendarToday, MenuBook} from "@material-ui/icons";
 
 
 function App() {
-  return (<>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">
-              And the dinner is...
-          </Typography>
-        </Toolbar>
-      </AppBar>
+    return <>
+        <BrowserRouter>
+            <AppBar position="static" className="app-bar">
+                <Toolbar>
+                    <Typography variant="h6">
+                        And the dinner is...
+                    </Typography>
 
-      <Container maxWidth="sm">
-          <Planner />
-      </Container>
-      </>
-  );
+                    <Link to="/">
+                        <IconButton edge="end">
+                            <CalendarToday/>
+                        </IconButton>
+                    </Link>
+
+                    <Link to="/dishes">
+                        <IconButton edge="end">
+                            <MenuBook/>
+                        </IconButton>
+                    </Link>
+                </Toolbar>
+            </AppBar>
+
+            <Container maxWidth="sm">
+                <Switch>
+                    <Route exact path="/" component={Planner} />
+
+                    <Route path="/dishes">
+                        <h1>Dishes</h1>
+                    </Route>
+                </Switch>
+            </Container>
+        </BrowserRouter>
+
+
+    </>;
 }
 
 export default App;
