c3fd7e60102d3fa896b1b988c38944fe078e4d2b
Alexander Malfait <alexander.malfait@catsolutions.be>
2021-03-11 22:30:39 +0100

    Add today button

--- src/planner/Planner.tsx
+++ src/planner/Planner.tsx
@@ -2,6 +2,7 @@ import React, {FC, useState} from "react";
 import {Box, Button} from "@material-ui/core";
 import {PlannerDate} from "./PlannerDate";
 import moment from "moment";
+import {SkipNext, SkipPrevious, Today} from "@material-ui/icons";
 
 export const Planner: FC = () => {
     const [currentMonday, setCurrentMonday] = useState(moment().day(1))
@@ -20,21 +21,36 @@ export const Planner: FC = () => {
         return weekDays;
     }
 
+    function setToday() {
+        setCurrentMonday(moment().day(1))
+    }
+
     function shiftWeek(delta: number) {
         setCurrentMonday(currentMonday.clone().add(delta, 'week'));
     }
 
     return <>
-        <Box p={3}>
+        <Box py={3}>
             {getWeek().map(day => <PlannerDate day={day}/>)}
         </Box>
 
-        <Button onClick={() => shiftWeek(-1)}>
-            Prev week
-        </Button>
+        <Box display="flex" justifyContent="space-between">
+            <Button onClick={() => shiftWeek(-1)} variant="contained">
+                <SkipPrevious />
+                Prev week
+            </Button>
 
-        <Button onClick={() => shiftWeek(1)}>
-            Next week
-        </Button>
+            <Button onClick={() => setToday()} variant="contained">
+                <Today />
+                Today
+            </Button>
+
+            <Button onClick={() => shiftWeek(1)} variant="contained" >
+                Next week
+                <SkipNext />
+            </Button>
+        </Box>
+
+
     </>
 }
