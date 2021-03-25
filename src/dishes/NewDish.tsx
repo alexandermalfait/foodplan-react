1e1010f0c57491752bfdd4d4f34e827aec6635ef
Alexander Malfait <alexander.malfait@catsolutions.be>
2021-03-25 20:55:40 +0100

    Basic Dish form

--- src/dishes/NewDish.tsx
+++ src/dishes/NewDish.tsx
@@ -1,40 +1,50 @@
-import {Button, FormControl, TextField} from "@material-ui/core";
+import {Button, createStyles, Grid, makeStyles, Paper, TextField} from "@material-ui/core";
 import React from "react";
-import {Controller, useForm} from "react-hook-form";
+import {useForm} from "react-hook-form";
+import {Link} from "react-router-dom";
 import {Dish} from "./Dish";
 
+const useStyles = makeStyles((theme) =>createStyles(({
+    paper: {
+        padding: theme.spacing(3)
+    },
+
+    buttons: {
+        marginTop: "1em",
+        display: "flex",
+        justifyContent: "space-between",
+        width: "100%"
+    }
+})))
+
 function DishForm() {
-    const {register, handleSubmit, control} = useForm()
+    const {register, handleSubmit } = useForm()
+
+    const classes = useStyles()
 
     const onSubmit = (dishy: Dish) => console.log(dishy)
 
     return <>
         <form onSubmit={handleSubmit(onSubmit)}>
-            <FormControl fullWidth>
-                <Controller
-                    name="name"
-                    as={
-                        <TextField label="Dish name" required={true} fullWidth/>
-                    }
-                    control={control}
-                    defaultValue=""
-                />
-            </FormControl>
+            <Paper className={classes.paper}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <TextField label="Dish name" required={true} inputRef={register} name={"name"} fullWidth/>
+                    </Grid>
 
-            <FormControl fullWidth>
-                <Controller
-                    name="url"
-                    as={
-                        <TextField label="Dish URL" type="url" fullWidth placeholder="http://some-url"/>
-                    }
-                    control={control}
-                    defaultValue=""
-                />
-            </FormControl>
+                    <Grid item xs={12}>
+                        <TextField label="Dish URL" type="url" fullWidth placeholder="http://some-url" inputRef={register} name="url" />
+                    </Grid>
 
-            <FormControl fullWidth>
-                <Button type="submit" variant="contained">Submit it!</Button>
-            </FormControl>
+                    <Grid item xs={12} className={classes.buttons}>
+                        <Button type="submit" variant="contained" color="primary">Save it</Button>
+
+                        <Button color="secondary">
+                            <Link to="/dishes">Cancel</Link>
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Paper>
         </form>
     </>;
 }
