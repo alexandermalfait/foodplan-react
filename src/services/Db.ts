1cda4d5ed8ec8507edf393e432e9fcb6b25b5773
Alexander Malfait <alexander.malfait@catsolutions.be>
2021-03-31 23:03:23 +0200

    Structure dishes collection per user

--- src/services/Db.ts
+++ src/services/Db.ts
@@ -7,32 +7,39 @@ export const db = firebaseApp.firestore()
 
 type User = firebase.User;
 
-function setUserId(record: { uid: string }, user: firebase.User) {
-    record.uid = user.uid
+function userDocument(user: User) {
+    return db.collection("users").doc(user.uid);
 }
 
 export function addDish(dish: Dish, user: User) {
-    setUserId(dish, user)
-
-    db.collection("dishes").add(dish)
+    userDocument(user).collection("dishes").add(dish)
 }
 
-export function snapshotDishes(callback:(dishes: Array<Dish>) => void): () => void {
-    return db
+export function updateDish(dish: Dish, user: User) {
+    userDocument(user).collection("dishes").doc(dish.id).update(dish)
+}
+
+function documentToDish(d: firebase.firestore.DocumentSnapshot<firebase.firestore.DocumentData>) {
+    return {...d.data(), id: d.id} as Dish;
+}
+
+export function snapshotDishes(user: firebase.User, callback:(dishes: Array<Dish>) => void): () => void {
+    return userDocument(user)
         .collection("dishes")
         .onSnapshot({
             next: snapshot => {
                 const dishes = snapshot.docs
-                    .map(d => {
-                        const dish = d.data() as Dish
-                        dish.id = d.id
-                        return dish;
-                    })
+                    .map(documentToDish)
                     .sort(sortBy("name^")); // case insensitive
 
                 return callback(dishes);
             },
-            error: () => alert("error")
+            error: (e) => console.error(e)
         })
 }
 
+export async function fetchDishById(user: firebase.User, dishId: string): Promise<Dish> {
+    const snapshot = await userDocument(user).collection("dishes").doc(dishId).get();
+
+    return documentToDish(snapshot)
+}
