3e370b3135964f85da230eb220725b937af3a5f4
Alexander Malfait <alexander.malfait@catsolutions.be>
2021-04-04 09:28:50 +0200

    useStyles for planner dates

--- src/planner/PlannerDate.tsx
+++ src/planner/PlannerDate.tsx
@@ -1,24 +1,45 @@
-import {Card} from "@material-ui/core";
+import {Card, createStyles, makeStyles} from "@material-ui/core";
 import moment, {Moment} from "moment";
-
-require("./PlannerDate.scss")
 
 interface Props {
     day: Moment
 }
 
-export function PlannerDate({ day } : Props) {
-    function isToday() {
-        return day.isSame(moment(), 'day');
+const useStyles = makeStyles(createStyles({
+    root: {
+        display: "flex",
+        minHeight: "50px",
+        margin: "1em 0"
+    } ,
+
+    date: {
+        padding: ".5em",
+        width: "15%",
+        background: "#d5eff6",
+        textAlign: "center",
+    } ,
+
+    dayOfWeek: {
+        fontWeight: "bold",
+    },
+
+    fullDate: {
+        fontSize: ".7em"
     }
+}))
+
+export function PlannerDate({ day } : Props) {
+    const classes = useStyles()
+
+    const isToday= day.isSame(moment(), 'day')
 
     return <>
-        <Card className="planner-date" raised={isToday()}>
-            <div className="date">
-                <div className={"day-of-week"}>
+        <Card className={classes.root} raised={isToday}>
+            <div className={classes.date}>
+                <div className={classes.dayOfWeek}>
                     {day.format("ddd")}
                 </div>
-                <div className={"full-date"}>
+                <div className={classes.fullDate}>
                     {day.format("D MMM")}
                 </div>
             </div>
